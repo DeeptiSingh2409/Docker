67b20730832f636182b495dd3ecf8e63ad44e5fc
DeeptiSingh2409 <thedeeptisingh@gmail.com>
2024-12-19 12:35:02 +0000

    first commit

---
+++ Pyspark/app.py
@@ -0,0 +1,13 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("AdditionApp").getOrCreate()
+
+a = 10
+b = 20
+
+result = a + b
+
+print(f"The result of {a} + {b} is {result}")
+
+spark.stop()
+
